73dbcca8571139a3bc06b8c50e4a834d04a3ed59
moznion <moznion@gmail.com>
2014-04-16 16:08:59 +0900

    Cosmetic fix

--- git-delete-merged-branches-with-remote.sh
+++ git-delete-merged-branches-with-remote.sh
@@ -12,9 +12,20 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
-git branch --merged $base_branch | xargs -n 1 | grep -Ev "^(\*|$base_branch)$" | xargs -n 1 git branch -d
+git branch --merged $base_branch              |
+  xargs -n 1 | grep -Ev "^(\*|$base_branch)$" |
+  xargs -n 1 git branch -d
 
-git fetch --prune
+git fetch --prune 2>&1 >/dev/null
 
-git branch -r --merged $base_branch | grep -v ' -> ' | sed 's/\([^\/]*\)\/\(.*\)/\1 :\2/' | grep -v ":$base_branch$" | xargs -n 2 -Itarget git push target 2>/dev/null
+git branch -r --merged $base_branch  |
+  grep -v ' -> '                     |
+  sed 's/\([^\/]*\)\/\(.*\)/\1 :\2/' |
+  grep -v ":$base_branch$"           |
+  xargs -n 2 git push 2>&1           | # All of outputs go to stderr
+  {
+    while read result; do
+      echo $result | grep -v '^error:' 2>&1 >/dev/null && echo $result
+    done
+  }
 
